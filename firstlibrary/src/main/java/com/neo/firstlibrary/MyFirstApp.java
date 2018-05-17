ce0f4d6683ecb855d660b0af852e69b75d2077a3
neo <working_neo@sina.com>
2018-05-17 15:59:32 +0800

    第一次提交所有本地到仓库

---
+++ firstlibrary/src/main/java/com/neo/firstlibrary/MyFirstApp.java
@@ -0,0 +1,17 @@
+package com.neo.firstlibrary;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2018/5/17.
+ */
+
+public class MyFirstApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.e("SSSSSSS","======outside==MyFirstApp==onCreate======");
+    }
+}
