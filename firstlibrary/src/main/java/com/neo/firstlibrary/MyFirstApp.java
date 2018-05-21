da06e9b90ca5a3992f7fda691628628b658d7d57
neo <working_neo@sina.com>
2018-05-21 17:22:01 +0800

    热修复02

--- firstlibrary/src/main/java/com/neo/firstlibrary/MyFirstApp.java
+++ firstlibrary/src/main/java/com/neo/firstlibrary/MyFirstApp.java
@@ -2,6 +2,7 @@ package com.neo.firstlibrary;
 
 import android.app.Application;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2018/5/17.
@@ -13,5 +14,7 @@ public class MyFirstApp extends Application {
         super.onCreate();
 
         Log.e("SSSSSSS","======outside==MyFirstApp==onCreate======");
+        Toast.makeText(this, "热修复成功", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "热修复", Toast.LENGTH_SHORT).show();
     }
 }
