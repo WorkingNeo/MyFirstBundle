5f4d621e8496efa72a2675a94b4cd3bdcb698b30
neo <working_neo@sina.com>
2018-05-24 07:11:08 +0800

    bundle0524提交新版本

--- firstlibrary/src/main/java/com/neo/firstlibrary/MainFirstActivity.java
+++ firstlibrary/src/main/java/com/neo/firstlibrary/MainFirstActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class MainFirstActivity extends AppCompatActivity {
 
-    private String tag ="three";
+    private String tag ="one";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,6 @@ public class MainFirstActivity extends AppCompatActivity {
 
     public void clickNullPoint(View view) {
         Log.e("SSSSS","======new NullPointerException=====");
-//        throw new NullPointerException();
+        throw new NullPointerException();
     }
 }
